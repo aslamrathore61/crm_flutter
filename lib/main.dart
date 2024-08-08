65cd7af0841dc7d94ac53e2a928d13e0383caf86
MohmmedAslamRathore <aslamrathore61@gmail.com>
2024-08-08 14:42:51 +0530

    git url update

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,6 @@ Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
  // If you're going to use other Firebase services in the background, such as Firestore,
  // make sure you call `initializeApp` before using other Firebase services.
 
-  print("HandlingBackgroundMessage: ${message.messageId}");
   Fluttertoast.showToast(
       msg: "${message.messageId} Notification",
       toastLength: Toast.LENGTH_SHORT,
